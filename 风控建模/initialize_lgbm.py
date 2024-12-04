7df9813a08118669313790ae73a275c92d067f8c
wujiang1999 <wujiang1999@sjtu.edu.cn>
2024-12-04 22:14:51 +0800

    修改了初始化lgbm模型的常用参数组合

--- 风控建模/initialize_lgbm.py
+++ 风控建模/initialize_lgbm.py
@@ -21,32 +21,28 @@ def initialize_lgbm(space):
 
 
 if __name__ == "__main__":
-    # lightgbm.LGBMClassifier(boosting_type='gbdt', 
-    #                         num_leaves=31, 
-    #                         max_depth=-1, 
-    #                         learning_rate=0.1, 
-    #                         n_estimators=100, 
-    #                         subsample_for_bin=200000, 
-    #                         objective=None, 
-    #                         class_weight=None, 
-    #                         min_split_gain=0.0, 
-    #                         min_child_weight=0.001, 
-    #                         min_child_samples=20, 
-    #                         subsample=1.0, 
-    #                         subsample_freq=0, 
-    #                         colsample_bytree=1.0, 
-    #                         reg_alpha=0.0, 
-    #                         reg_lambda=0.0, 
-    #                         random_state=None, 
-    #                         n_jobs=None, 
-    #                         importance_type='split', 
-    #                         **kwargs)
     space = {
-        'n_estimators': 100,
-        'learning_rate': 0.05,
-        'max_depth': 7,
+        # 'boosting_type':'gbdt',
+        'num_leaves':31,
+        'max_depth':3, 
+        'learning_rate':0.02,
+        'n_estimators':1000,
+        # 'subsample_for_bin':200000,
+        'objective':'binary',
+        'class_weight':None,
+        'min_split_gain':0.0,
+        'min_child_weight':1e-3,
+        'min_child_samples':500,
+        'subsample':0.9,
+        'subsample_freq':100,
+        'colsample_bytree':0.9,
+        'reg_alpha':0.0,
+        'reg_lambda':0.0,
+        'random_state':823,
+        'n_jobs':-1,
+        # 'importance_type':'split'
     }
 
     model = initialize_lgbm(space)
     print(dir(model))
-    print(model.n_estimators)
+    print(model)
